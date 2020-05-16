eb0f37345314c6502897ef51abb24baccab924b9
徳永厚友 <>
2020-05-16 23:25:04 +0900

    Update two filesone layout second kotlin

--- app/src/main/java/com/app1/androidwithgithub/MainActivity.kt
+++ app/src/main/java/com/app1/androidwithgithub/MainActivity.kt
@@ -2,11 +2,19 @@ package com.app1.androidwithgithub
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var text = findViewById<TextView>(R.id.text_view)
+        val btn = findViewById<Button>(R.id.btn)
+        val image = findViewById<ImageView>(R.id.image)
+
     }
 }
